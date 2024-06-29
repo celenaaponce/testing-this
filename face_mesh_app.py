03e750c85c02ac0d1ae2072e8b7a60099f80d5ba
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-29 09:40:17 -0700

    Update face_mesh_app.py

--- face_mesh_app.py
+++ face_mesh_app.py
@@ -13,32 +13,6 @@ DEMO_VIDEO = 'demo/demo.mp4'
 # Basic App Scaffolding
 st.title('Face Mesh App using Streamlit')
 
-## Add Sidebar and Main Window style
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 350px
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 350px
-        margin-left: -350px
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-## Create Sidebar
-st.sidebar.title('FaceMesh Sidebar')
-st.sidebar.subheader('Parameter')
-
-## Define available pages in selection box
-app_mode = st.sidebar.selectbox(
-    'App Mode',
-    ['About','Image','Video']
-)
-
 # Resize Images to fit Container
 @st.cache()
 # Get Image Dimensions
@@ -66,110 +40,9 @@ def image_resize(image, width=None, height=None, inter=cv.INTER_AREA):
 
     return resized
 
-
-# About Page
-
-if app_mode == 'About':
-    st.markdown('''
-                ## Face Mesh \n
-                In this application we are using **MediaPipe** for creating a Face Mesh. **StreamLit** is to create 
-                the Web Graphical User Interface (GUI) \n
-                
-                - [Github](https://github.com/mpolinowski/streamLit-cv-mediapipe) \n
-    ''')
-
-    ## Add Sidebar and Window style
-    st.markdown(
-        """
-        <style>
-        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-            width: 350px
-        }
-        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-            width: 350px
-            margin-left: -350px
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
-# Image Page
-
-elif app_mode == 'Image':
-    drawing_spec = mp.solutions.drawing_utils.DrawingSpec(thickness=2, circle_radius=1)
-
-    st.sidebar.markdown('---')
-
-    ## Add Sidebar and Window style
-    st.markdown(
-        """
-        <style>
-        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-            width: 350px
-        }
-        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-            width: 350px
-            margin-left: -350px
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
-    st.markdown("**Detected Faces**")
-    kpil_text = st.markdown('0')
-
-    max_faces = st.sidebar.number_input('Maximum Number of Faces', value=2, min_value=1)
-    st.sidebar.markdown('---')
-
-    detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value=0.0,max_value=1.0,value=0.5)
-    st.sidebar.markdown('---')
-
-    ## Output
-    st.markdown('## Output')
-    img_file_buffer = st.sidebar.file_uploader("Upload an Image", type=["jpg","jpeg","png"])
-    if img_file_buffer is not None:
-        image = np.array(Image.open(img_file_buffer))
-
-    else:
-        demo_image = DEMO_IMAGE
-        image = np.array(Image.open(demo_image))
-
-    st.sidebar.text('Original Image')
-    st.sidebar.image(image)
-
-    face_count=0
-
-    ## Dashboard
-    with mp.solutions.face_mesh.FaceMesh(
-        static_image_mode=True, #Set of unrelated images
-        max_num_faces=max_faces,
-        min_detection_confidence=detection_confidence
-    ) as face_mesh:
-
-            results = face_mesh.process(image)
-            out_image=image.copy()
-
-            #Face Landmark Drawing
-            for face_landmarks in results.multi_face_landmarks:
-                face_count += 1
-
-                mp.solutions.drawing_utils.draw_landmarks(
-                    image=out_image,
-                    landmark_list=face_landmarks,
-                    connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
-                    landmark_drawing_spec=drawing_spec
-                )
-
-                kpil_text.write(f"<h1 style='text-align: center; color:red;'>{face_count}</h1>", unsafe_allow_html=True)
-
-            st.subheader('Output Image')
-            st.image(out_image, use_column_width=True)
-
 # Video Page
 
-elif app_mode == 'Video':
+if True:
 
     st.set_option('deprecation.showfileUploaderEncoding', False)
 
