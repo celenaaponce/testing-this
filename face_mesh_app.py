43ce0c311e85bdc960760a25d2b44fc1068adb01
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-29 10:10:01 -0700

    Update face_mesh_app.py

--- face_mesh_app.py
+++ face_mesh_app.py
@@ -42,92 +42,89 @@ def image_resize(image, width=None, height=None, inter=cv.INTER_AREA):
 
 # Video Page
 
-if True:
 
-    st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-    use_webcam = st.sidebar.button('Use Webcam')
+use_webcam = True
 
-    drawing_spec = mp.solutions.drawing_utils.DrawingSpec(thickness=2, circle_radius=1)
+drawing_spec = solutions.drawing_utils.DrawingSpec(thickness=2, circle_radius=1)
 
-    ## Get Video
-    stframe = st.empty()
-    # video_file_buffer = st.sidebar.file_uploader("Upload a Video", type=['mp4', 'mov', 'avi', 'asf', 'm4v'])
-    # temp_file = tempfile.NamedTemporaryFile(delete=False)
-    img_file_buffer = st.camera_input("Take a picture")
-    print('none', img_file_buffer == None)
-    if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-        bytes_data = img_file_buffer.getvalue()
-        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+## Get Video
+stframe = st.empty()
 
+video = cv.VideoCapture(0)
 
-    drawing_spec = mp.solutions.drawing_utils.DrawingSpec(thickness=2, circle_radius=1)
+width = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
+fps_input = int(video.get(cv.CAP_PROP_FPS))
 
-    kpil, kpil2, kpil3 = st.columns(3)
+fps = 0
+i = 0
 
-    with kpil:
-        st.markdown('**Frame Rate**')
-        kpil_text = st.markdown('0')
+kpil, kpil2, kpil3 = st.columns(3)
 
-    with kpil2:
-        st.markdown('**Detected Faces**')
-        kpil2_text = st.markdown('0')
+with kpil:
+    st.markdown('**Frame Rate**')
+    kpil_text = st.markdown('0')
 
-    with kpil3:
-        st.markdown('**Image Resolution**')
-        kpil3_text = st.markdown('0')
+with kpil2:
+    st.markdown('**Detected Faces**')
+    kpil2_text = st.markdown('0')
 
-    st.markdown('<hr/>', unsafe_allow_html=True)
+with kpil3:
+    st.markdown('**Image Resolution**')
+    kpil3_text = st.markdown('0')
+
+st.markdown('<hr/>', unsafe_allow_html=True)
 
 
-    ## Face Mesh
-    with mp.solutions.holistic.Holistic(
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.5
-    ) as holistic:
+## Face Mesh
+with mp.solutions.holistic.Holistic(
+min_detection_confidence=0.7,
+min_tracking_confidence=0.5
+) as holistic:
 
-            prevTime = 0
+        prevTime = 0
 
-            while img_file_buffer is not None:
-                st.write('here')
-                i +=1
-                ret, frame = cv2_img.read()
-                st.write(cv2_img.shape)
-                if not ret:
-                    continue
+        while img_file_buffer is not None:
+            st.write('here')
+            i +=1
+            ret, frame = cv2_img.read()
+            st.write(cv2_img.shape)
+            if not ret:
+                continue
 
-                results = holistic.process(frame)
-                frame.flags.writeable = True
-                left_present = dominant_hand == 'LEFT' and results.left_hand_landmarks is not None
-                right_present = dominant_hand == 'RIGHT' and results.right_hand_landmarks is not None
-                face_count = 0
-                if results.pose_landmarks is not None and left_present or right_present and not success:
-    
-                    #Face Landmark Drawing
-                    for face_landmarks in results.pose_landmarks.landmark:
-    
-                        solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, solutions.holistic.POSE_CONNECTIONS, 
-                                solutions.drawing_utils.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),  
-                                solutions.drawing_utils.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2) 
-                                ) 
+            results = holistic.process(frame)
+            frame.flags.writeable = True
+            left_present = dominant_hand == 'LEFT' and results.left_hand_landmarks is not None
+            right_present = dominant_hand == 'RIGHT' and results.right_hand_landmarks is not None
+            face_count = 0
+            if results.pose_landmarks is not None and left_present or right_present and not success:
+
+                #Face Landmark Drawing
+                for face_landmarks in results.pose_landmarks.landmark:
+
+                    solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, solutions.holistic.POSE_CONNECTIONS, 
+                            solutions.drawing_utils.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),  
+                            solutions.drawing_utils.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2) 
+                            ) 
 
 
-                # FPS Counter
-                currTime = time.time()
-                fps = 1/(currTime - prevTime)
-                prevTime = currTime
+            # FPS Counter
+            currTime = time.time()
+            fps = 1/(currTime - prevTime)
+            prevTime = currTime
 
-                if record:
-                    out.write(frame)
+            if record:
+                out.write(frame)
 
-                # Dashboard
-                kpil_text.write(f"<h1 style='text-align: center; color:red;'>{int(fps)}</h1>", unsafe_allow_html=True)
-                kpil2_text.write(f"<h1 style='text-align: center; color:red;'>{face_count}</h1>", unsafe_allow_html=True)
-                kpil3_text.write(f"<h1 style='text-align: center; color:red;'>{width*height}</h1>",
-                                 unsafe_allow_html=True)
+            # Dashboard
+            kpil_text.write(f"<h1 style='text-align: center; color:red;'>{int(fps)}</h1>", unsafe_allow_html=True)
+            kpil2_text.write(f"<h1 style='text-align: center; color:red;'>{face_count}</h1>", unsafe_allow_html=True)
+            kpil3_text.write(f"<h1 style='text-align: center; color:red;'>{width*height}</h1>",
+                             unsafe_allow_html=True)
 
-                frame = cv.resize(frame,(0,0), fx=0.8, fy=0.8)
-                frame = image_resize(image=frame, width=640)
-                stframe.image(frame,channels='BGR', use_column_width=True)
+            frame = cv.resize(frame,(0,0), fx=0.8, fy=0.8)
+            frame = image_resize(image=frame, width=640)
+            stframe.image(frame,channels='BGR', use_column_width=True)
 
