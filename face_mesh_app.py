d990bd458efbaadb9185dba44c14e76f4b9d1c3e
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-29 11:31:34 -0700

    Update face_mesh_app.py

--- face_mesh_app.py
+++ face_mesh_app.py
@@ -6,45 +6,45 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     st.write(image)
 
     # Run inference
-    # blob = cv2.dnn.blobFromImage(
-    #     cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
-    # )
-    # net.setInput(blob)
-    # output = net.forward()
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+    net.setInput(blob)
+    output = net.forward()
 
     h, w = image.shape[:2]
 
     # Convert the output array into a structured form.
     output = output.squeeze()  # (1, 1, N, 7) -> (N, 7)
     output = output[output[:, 2] >= score_threshold]
-    # detections = [
-    #     Detection(
-    #         class_id=int(detection[1]),
-    #         label=CLASSES[int(detection[1])],
-    #         score=float(detection[2]),
-    #         box=(detection[3:7] * np.array([w, h, w, h])),
-    #     )
-    #     for detection in output
-    # ]
+    detections = [
+        Detection(
+            class_id=int(detection[1]),
+            label=CLASSES[int(detection[1])],
+            score=float(detection[2]),
+            box=(detection[3:7] * np.array([w, h, w, h])),
+        )
+        for detection in output
+    ]
 
-    # Render bounding boxes and captions
-    # for detection in detections:
-    #     caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
-    #     color = COLORS[detection.class_id]
-    #     xmin, ymin, xmax, ymax = detection.box.astype("int")
+    Render bounding boxes and captions
+    for detection in detections:
+        caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
+        color = COLORS[detection.class_id]
+        xmin, ymin, xmax, ymax = detection.box.astype("int")
 
-    #     cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-    #     cv2.putText(
-    #         image,
-    #         caption,
-    #         (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
-    #         cv2.FONT_HERSHEY_SIMPLEX,
-    #         0.5,
-    #         color,
-    #         2,
-    #     )
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+        cv2.putText(
+            image,
+            caption,
+            (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            color,
+            2,
+        )
 
-    # result_queue.put(detections)
+    result_queue.put(detections)
 
     return av.VideoFrame.from_ndarray(image, format="bgr24")
 st.title("OpenCV Filters on Video Stream")
