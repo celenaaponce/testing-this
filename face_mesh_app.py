5e394a2471d5a127a92e43f773081176b50a0ffd
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-29 18:29:10 -0700

    Update face_mesh_app.py

--- face_mesh_app.py
+++ face_mesh_app.py
@@ -1,41 +1,6 @@
-import threading
-
-import cv2
-import streamlit as st
-from matplotlib import pyplot as plt
-
 from streamlit_webrtc import webrtc_streamer
-import logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
-logger.debug(f"Socket: {self._sock}, Transport: {self.transport}, Loop: {self._loop}")
-lock = threading.Lock()
-img_container = {"img": None}
-
-
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    with lock:
-        img_container["img"] = img
-
-    return frame
-
-
-ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
-
-fig_place = st.empty()
-fig, ax = plt.subplots(1, 1)
-
-while ctx.state.playing:
-    with lock:
-        img = img_container["img"]
-    if img is None:
-        continue
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ax.cla()
-    ax.hist(gray.ravel(), 256, [0, 256])
-    fig_place.pyplot(fig)
+webrtc_streamer(key="sample")
 
 
 # import streamlit as st
